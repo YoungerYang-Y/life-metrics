434e0e951cab225a73005566569b7a2cf8650428
YoungerYang <1059376437@qq.com>
2025-03-27 23:59:49 +0800

    Initial commit

---
+++ src/pages/about.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+  <RouterLink to="/">
+    <button>
+      Go back index
+    </button>
+  </RouterLink>
+</template>
+
+<style scoped>
+</style>
+
+<route lang="yaml">
+{
+  meta: {
+    layout: 'about'
+  }
+}
+</route>
